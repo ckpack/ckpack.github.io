5773e5c9a72be306078125822c727dc8c89ee477
陈锴 <k_0520@qq.com>
2024-07-08 17:12:49 +0800

    save

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Aura from '@primevue/themes/aura';
 import { router } from './router.js'
 import 'uno.css';
 import './style/base.scss'
+import 'tdesign-vue-next/es/style/index.css';
 import App from './App.vue'
 
 const app = createApp(App)
