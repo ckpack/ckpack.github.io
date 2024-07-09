f4a5cf078e38b9c8819bd9a15a511d0f0da7ec4e
陈锴 <k_0520@qq.com>
2024-07-09 09:54:27 +0800

    Update the-menu.vue

--- src/components/the-menu.vue
+++ src/components/the-menu.vue
@@ -58,15 +58,13 @@ const items = ref([
         <template #item="{ item, props, hasSubmenu, root }">
           <a class="flex items-center" v-bind="props.action">
             <span :class="item.icon" />
-            <t-button v-if="item.type === 'button'">{{ item.label }}</t-button>
-            <template v-else>
-              <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-                <a :href="href" v-bind="props.action" @click="navigate">
-                  <span class="ml-2">{{ item.label }}</span>
-                </a>
-              </router-link>
-              <span v-else class="ml-2">{{ item.label }}</span>
-            </template>
+            <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+              <a :href="href" v-bind="props.action" @click="navigate">
+                <t-button v-if="item.type === 'button'">{{ item.label }}</t-button>
+                <span v-else>{{ item.label }}</span>
+              </a>
+            </router-link>
+            <span v-else>{{ item.label }}</span>
             <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
             <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
             <i v-if="hasSubmenu" class="pi pi-angle-down" :class="[{ 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]" />
